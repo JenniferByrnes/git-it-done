6d84f001684e0291bcb8eb2d67e078ad879c47d2
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-06-27 11:20:40 -0400

    added issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,13 +2,13 @@ var issueContainerEl = document.querySelector("#issues-container");
 
 var getRepoIssues = function(repo) {
   
-
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
   fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
+        // pass response data to dom function
         displayIssues(data)
       });
     }
@@ -19,6 +19,11 @@ var getRepoIssues = function(repo) {
 ;}
 
 var displayIssues = function(issues) {
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  };
+
   for (var i=0; i<issues.length; i++) {
     // create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
@@ -44,10 +49,10 @@ var displayIssues = function(issues) {
     }
 
     // append to container
-    issueEl.appendChild(typeEl);
+    issueEl.appendChild(typeEl); 
     issueContainerEl.appendChild(issueEl);
 
   }
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues("JenniferByrnes/git-it-done");
